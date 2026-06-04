9b4c81bb976e6cd4870f7fa9d84636375a0a1396
szubixyt-dev <szubixyt@gmail.com>
2026-06-04 21:26:33 +0200

    Add files via upload

---
+++ PakietApki.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 65001 >nul
+echo Instalowanie...
+winget install "Google.Chrome"
+winget install "Proton.ProtonMail"
+winget install "Discord.Discord"
+winget install "dotPDN.PaintDotNet"
+winget install "GIMP.GIMP"
+winget install "Notepad++.Notepad++"
+winget install "Python.Python.3.9"
+winget install "VideoLan.VLC"
+winget install "7zip.7zip"
+winget install "XP8BX2DWV7TF50"
+exit
